ab426bf47cb3d374396dd45e51317f8357b68f2c
AKXRSH <134067749+akarsh0913@users.noreply.github.com>
2023-09-25 00:03:55 +0530

    Create 02 - DataTypes.cs

---
+++ C#/02 - DataTypes.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Have a good day!";
+            char grade = 'A'; // char for single characters
+            int age = 30; // int doesn't require quotation marks
+            int num = -1;
+            // There are three data types for decimals: float, double, decimal (less precise to most precise)
+            float a = 3.21;
+            double b = 0.2;
+            decimal c = 3.444;
+            bool enrolStatus = true;
+            // bool enrolStatus = false;
+
+            Console.ReadLine();
+        }
+    }
+}
