3858021a9daf70ca54bfb7edf732df8af85654b4
AKXRSH <134067749+akarsh0913@users.noreply.github.com>
2023-09-25 00:02:57 +0530

    Create Variables.cs

---
+++ C#/Variables.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+            
+            Console.WriteLine("There once was a man named " + characterName + ".");
+            Console.WriteLine("He was " + characterAge + " years old.");
+            Console.WriteLine("He really liked the name of George.");
+            Console.WriteLine("But didn't like being 70.");
+
+            Console.ReadLine();
+        }
+    }
+}
