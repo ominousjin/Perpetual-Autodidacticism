d6a4c672b07ca90269584201862e5dca9c6060b1
AKXRSH <134067749+akarsh0913@users.noreply.github.com>
2023-09-25 00:07:33 +0530

    Create 06 - StringToInput.cs

---
+++ C#/06 - StringToInput.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Converting string value to an integer value
+            int num = Convert.ToInt32("45");
+            Console.WriteLine(num + 6);
+
+            Console.ReadLine();
+
+        }
+    }
+}
