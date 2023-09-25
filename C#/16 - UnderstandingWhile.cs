9935e42abc29bbdeb69c61d2eb9828823662c6a7
AKXRSH <134067749+akarsh0913@users.noreply.github.com>
2023-09-25 21:48:38 +0530

    Update 16 - UnderstandingWhile.cs

--- C#/16 - UnderstandingWhile.cs
+++ C#/16 - UnderstandingWhile.cs
@@ -11,13 +11,27 @@ namespace App1
         static void Main(string[] args)
         {
             // A while loop repeatedly executes a set of instructions as long as a specified condition is true.
-            int i = 0;
+            // int i = 0;
+            // Conditions are passed as parameters (boolean values allow looping)
+            // while (i < 5) 
+            // {
+            //     Console.WriteLine("Test Succeeded");
+            //     Increment operator increments the value of variable until the specified condition becomes false.
+            //     i++; 
+            // }
 
-            while (i < 5) // Accepted parameters are only boolean values
+            int index = 1;
+
+            while(index <= 5)
             {
-                Console.WriteLine("Test Succeeded");
-                i++; // Increment operator increments the value of variable until the specified condition becomes false.
+                Console.WriteLine(index);
+                index++;
             }
+
+            // Infinite loop - a condition that never becomes false
+            // So in previous example, if we remove 'index++;' then the condition will always be true.
+            // And this is because we aren't modifying the value of index variable anymore.
+            // And since this becomes the case, the while loop example will never stop printing value of index variable.
 
             Console.ReadLine();
         }
