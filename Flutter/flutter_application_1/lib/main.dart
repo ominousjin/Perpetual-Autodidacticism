090861f89f333da9f6df8b3f13c23ce5baea5234
AKXRSH <akarsh.mishra0913@gmail.com>
2023-12-22 22:18:30 +0530

    Programming Fundamentals
    Variables, Basic Math Operators, Comparison Operators, Logical Operators, Control Flow, Switch Statement

--- Flutter/flutter_application_1/lib/main.dart
+++ Flutter/flutter_application_1/lib/main.dart
@@ -1,125 +1,149 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
-  // This widget is the root of your application.
+  // 1. PROGRAMMING FUNDAMENTALS
+
+  // VARIABLES: You can store different types of info into variables
+  String name = "Sebastian";
+  int age = 21;
+  double pi = 3.14159;
+  bool isBeginner = true;
+
+  /*
+    BASIC MATH OPERATORS
+    4 + 4 = 8, add
+    5 - 3 = 2, subtract
+    2 * 3 = 6, multiply
+    9 / 3 = 3, divide
+    9 % 4 = 1, remainder
+  */
+
+  /*
+    COMPARISON OPERATORS
+    5 == 5 -> true,   EQUAL TO
+    2 != 3 -> true,   NOT EQUAL TO
+    3 > 2  -> true,   GREATER THAN
+    3 < 2  -> false,  LESS THAN
+    5 >= 5 -> true,   GREATER OR EQUAL TO
+    3 <= 7 -> true,   LESS OR EQUAL TO 
+  */
+
+  /*
+    LOGICAL OPERATORS
+
+    AND operator, returns true if both sides are true
+    isBeginner && ( age < 18 ) -> returns false
+
+    OR operator, returns true if at least one side is true
+    isBeginner || ( age < 18 ) -> returns true
+
+    NOT operator, returns the opposite value
+    !isBeginner -> returns false
+  */
+
+  /*
+    CONTROL FLOW
+
+    if (condition) {
+      do something
+    }
+
+    if (condition) {
+      do something
+    } else if (another condition) {
+      do something else
+    } else {
+      do something else
+    }
+
+    if (condition) {
+      do something
+    } else {
+      do something else
+    }
+  */
+
+  /*
+    Instead of using repetitve if statements, for e.g. a grading system, we can use switch statement.
+
+    switch (expression) {
+      case value:
+        
+        break;
+      default:
+    }
+  */
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+    /*
+      EXAMPLES OF CONTROL FLOW
+      ------------------------------------------------------------------------
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+      if (age >= 18) {
+        print("You are an adult.");
+      } else {
+        print("You are not an adult.");
+      }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+      if (age < 13) {
+        print("You can only watch G rated movies.");
+      } else if (age < 18) {
+        print("You can only watch G and PG13 rated movies.");
+      } else {
+        print("You can watch G, PG13 and R rated movies.");
+      }
+    */
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+    /*
+      EXAMPLE OF SWITCH STATEMENT
+      ------------------------------------------------------------------------
 
-  final String title;
+      String grade = "B";
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+      Typical if statement
+      ----------------------------------
+      if (grade == "A") {
+        print("Excellent!");
+      } else if (grade == "B") {
+        print("Good!");
+      } else if (grade == "C") {
+        print("Fair!");
+      } else if (grade == "D") {
+        print("Needs Improvement!");
+      } else {
+        print("Invalid Grade");
+      }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+      Switch statement
+      ----------------------------------
+      switch (grade) {
+        case "A":
+          print("Excellent!");
+          break;
+        case "B":
+          print("Good!");
+          break;
+        case "C":
+          print("Fair!");
+          break;
+        case "D":
+          print("Needs Improvement!");
+          break;
+        default:
+          print("Invalid Grade");
+      }
+    */
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
     );
   }
 }
